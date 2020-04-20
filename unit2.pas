e3f6d96c834b94b9524394a51b421f0fccb11974
shaoziyang <shaoziyang@126.com>
2020-04-21 00:14:48 +0800

    add options for distance convert and Decimal Multiple

--- unit2.pas
+++ unit2.pas
@@ -40,8 +40,8 @@ type
     lbLuaWebSite: TLabel;
     lbTinyCWebSite1: TLabel;
     lbTinyCWebSite2: TLabel;
-    PageControl1: TPageControl;
-    Panel1: TPanel;
+    pcOption: TPageControl;
+    pnlButton: TPanel;
     Panel2: TPanel;
     TabSheet1: TTabSheet;
     TabSheet2: TTabSheet;
@@ -50,6 +50,7 @@ type
     procedure btnOptionOKClick(Sender: TObject);
     procedure btnOptionSelectFontClick(Sender: TObject);
     procedure chkShowTrayChange(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
     procedure lbLuaWebSiteClick(Sender: TObject);
     procedure lbMPYWebSiteClick(Sender: TObject);
@@ -92,26 +93,19 @@ begin
 end;
 
 procedure TFormOption.btnOptionOKClick(Sender: TObject);
+var
+  i: integer;
+  s: string;
 begin
-  ini.WriteBool('Option', 'CRC_Enabled', tvFunctions.Items[0].StateIndex = 1);
-  ini.WriteBool('Option', 'Dital_Enabled', tvFunctions.Items[1].StateIndex = 1);
-  ini.WriteBool('Option', 'Bytes_Enabled', tvFunctions.Items[2].StateIndex = 1);
-  ini.WriteBool('Option', 'Base_Enabled', tvFunctions.Items[3].StateIndex = 1);
-  ini.WriteBool('Option', 'BigFloat_Enabled', tvFunctions.Items[4].StateIndex = 1);
-  ini.WriteBool('Option', 'BigInt_Enabled', tvFunctions.Items[5].StateIndex = 1);
-  ini.WriteBool('Option', 'Convert_Enabled', tvFunctions.Items[6].StateIndex = 1);
-  ini.WriteBool('Option', 'Time_Enabled', tvFunctions.Items[7].StateIndex = 1);
-  ini.WriteBool('Option', 'Calc_Enabled', tvFunctions.Items[8].StateIndex = 1);
-  ini.WriteBool('Option', 'Script_Enabled', tvFunctions.Items[9].StateIndex = 1);
-  ini.WriteBool('Option', 'Pascal_Enabled', tvFunctions.Items[10].StateIndex = 1);
-  ini.WriteBool('Option', 'micropython_Enabled', tvFunctions.Items[11].StateIndex = 1);
-  ini.WriteBool('Option', 'Lua_Enabled', tvFunctions.Items[12].StateIndex = 1);
-  ini.WriteBool('Option', 'C_Enabled', tvFunctions.Items[13].StateIndex = 1);
-  ini.WriteBool('Option', 'Constant_Enabled', tvFunctions.Items[14].StateIndex = 1);
-  ini.WriteBool('Option', 'ConstantMath_Enabled', tvFunctions.Items[15].StateIndex = 1);
-  ini.WriteBool('Option', 'ConstantPhysics_Enabled',
-    tvFunctions.Items[16].StateIndex = 1);
-
+  for i := 0 to tvFunctions.Items.Count - 1 do
+  begin
+    if tvFunctions.Items[i].Parent = nil then
+      s := ''
+    else
+      s := tvFunctions.Items[i].Parent.Text + '_';
+    ini.WriteBool('Enabled', s + tvFunctions.Items[i].Text,
+      tvFunctions.Items[i].StateIndex = 1);
+  end;
   FormMain.updateTabVisible;
 
 end;
@@ -122,9 +116,14 @@ begin
   chkCloseToTray.Enabled    := chkShowTray.Checked;
 end;
 
+procedure TFormOption.FormCreate(Sender: TObject);
+begin
+  pcOption.ActivePageIndex:=0;
+end;
+
 procedure TFormOption.FormShow(Sender: TObject);
 var
-  node: TTreeNode;
+  s: string;
   i: integer;
 begin
   Font := FormMain.Font;
@@ -138,25 +137,15 @@ begin
   chkCloseToTray.Checked := FormMain.CloseToTray;
   chkErrlog_En.Checked   := FormMain.errlog_en;
 
-  tvSetCheck(tvFunctions.Items[0], ini.ReadBool('Option', 'CRC_Enabled', True));
-  tvSetCheck(tvFunctions.Items[1], ini.ReadBool('Option', 'Dital_Enabled', True));
-  tvSetCheck(tvFunctions.Items[2], ini.ReadBool('Option', 'Bytes_Enabled', True));
-  tvSetCheck(tvFunctions.Items[3], ini.ReadBool('Option', 'Base_Enabled', True));
-  tvSetCheck(tvFunctions.Items[4], ini.ReadBool('Option', 'BigFloat_Enabled', True));
-  tvSetCheck(tvFunctions.Items[5], ini.ReadBool('Option', 'BigInt_Enabled', True));
-  tvSetCheck(tvFunctions.Items[6], ini.ReadBool('Option', 'Convert_Enabled', True));
-  tvSetCheck(tvFunctions.Items[7], ini.ReadBool('Option', 'Time_Enabled', True));
-  tvSetCheck(tvFunctions.Items[8], ini.ReadBool('Option', 'Calc_Enabled', True));
-  tvSetCheck(tvFunctions.Items[9], ini.ReadBool('Option', 'Script_Enabled', True));
-  tvSetCheck(tvFunctions.Items[10], ini.ReadBool('Option', 'Pascal_Enabled', True));
-  tvSetCheck(tvFunctions.Items[11], ini.ReadBool('Option', 'micropython_Enabled', True));
-  tvSetCheck(tvFunctions.Items[12], ini.ReadBool('Option', 'Lua_Enabled', True));
-  tvSetCheck(tvFunctions.Items[13], ini.ReadBool('Option', 'C_Enabled', True));
-  tvSetCheck(tvFunctions.Items[14], ini.ReadBool('Option', 'Constant_Enabled', True));
-  tvSetCheck(tvFunctions.Items[15], ini.ReadBool('Option',
-    'ConstantMath_Enabled', True));
-  tvSetCheck(tvFunctions.Items[16], ini.ReadBool('Option',
-    'ConstantPhysics_Enabled', True));
+  for i := 0 to tvFunctions.Items.Count - 1 do
+  begin
+    if tvFunctions.Items[i].Parent = nil then
+      s := ''
+    else
+      s := tvFunctions.Items[i].Parent.Text + '_';
+    tvSetCheck(tvFunctions.Items[i], ini.ReadBool('Enabled',
+      s + tvFunctions.Items[i].Text, True));
+  end;
 
   chkUse_external_micropython.Checked := FormMain.use_external_micropython;
   edtExternal_micropython_bin.Text := FormMain.external_micropython_bin_name;
